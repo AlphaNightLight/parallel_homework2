49da81919da5c6012c39f0547a9a1e6f93faa48c
Alex Pegoraro <wikialex02@gmail.com>
2023-11-21 12:20:35 +0100

    prova_cluster_2

--- compile.sh
+++ compile.sh
@@ -10,22 +10,22 @@ fi
 
 
 
-g++ -o serial/bin/matMul_dense.exe serial/matMul_dense.cpp
-g++ -o serial/bin/matMul_sparse.exe serial/matMul_sparse.cpp
+g++ -std=gnu++11 -o serial/bin/matMul_dense.exe serial/matMul_dense.cpp
+g++ -std=gnu++11 -o serial/bin/matMul_sparse.exe serial/matMul_sparse.cpp
 
-g++ -o serial/bin/matT_dense.exe serial/matT_dense.cpp
-g++ -o serial/bin/matT_sparse.exe serial/matT_sparse.cpp
+g++ -std=gnu++11 -o serial/bin/matT_dense.exe serial/matT_dense.cpp
+g++ -std=gnu++11 -o serial/bin/matT_sparse.exe serial/matT_sparse.cpp
 
-g++ -o serial/bin/matBlockT_dense.exe serial/matBlockT_dense.cpp
-g++ -o serial/bin/matBlockT_sparse.exe serial/matBlockT_sparse.cpp
+g++ -std=gnu++11 -o serial/bin/matBlockT_dense.exe serial/matBlockT_dense.cpp
+g++ -std=gnu++11 -o serial/bin/matBlockT_sparse.exe serial/matBlockT_sparse.cpp
 
 
 
-g++ -fopenmp -o parallel/bin/matMulPar_dense.exe parallel/matMulPar_dense.cpp
-g++ -fopenmp -o parallel/bin/matMulPar_sparse.exe parallel/matMulPar_sparse.cpp
+g++ -std=gnu++11 -fopenmp -o parallel/bin/matMulPar_dense.exe parallel/matMulPar_dense.cpp
+g++ -std=gnu++11 -fopenmp -o parallel/bin/matMulPar_sparse.exe parallel/matMulPar_sparse.cpp
 
-g++ -fopenmp -o parallel/bin/matTpar_dense.exe parallel/matTpar_dense.cpp
-g++ -fopenmp -o parallel/bin/matTpar_sparse.exe parallel/matTpar_sparse.cpp
+g++ -std=gnu++11 -fopenmp -o parallel/bin/matTpar_dense.exe parallel/matTpar_dense.cpp
+g++ -std=gnu++11 -fopenmp -o parallel/bin/matTpar_sparse.exe parallel/matTpar_sparse.cpp
 
-g++ -fopenmp -o parallel/bin/matBlockTpar_dense.exe parallel/matBlockTpar_dense.cpp
-g++ -fopenmp -o parallel/bin/matBlockTpar_sparse.exe parallel/matBlockTpar_sparse.cpp
+g++ -std=gnu++11 -fopenmp -o parallel/bin/matBlockTpar_dense.exe parallel/matBlockTpar_dense.cpp
+g++ -std=gnu++11 -fopenmp -o parallel/bin/matBlockTpar_sparse.exe parallel/matBlockTpar_sparse.cpp
